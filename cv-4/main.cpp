7cd3e4758462184024da631553870122618bd2e5
denvery31 <denvery31@gmail.com>
2024-03-20 22:44:11 +0100

    cv-4.2

--- cv-4/main.cpp
+++ cv-4/main.cpp
@@ -48,12 +48,16 @@ Account::Account(int n, Client *c) {
     this->number = n;
     this->owner = c;
     this->partner = nullptr;
+    this->interestRate = 0.05;
+    this->balance =0;
 }
 
 Account::Account(int n, Client *c, Client *p) {
     this->number = n;
     this->owner =c;
     this->partner = p;
+    this->interestRate = 0.05;
+    this->balance =0;
 }
 
 Account::Account(int n, Client *c, double ir) {
@@ -61,6 +65,7 @@ Account::Account(int n, Client *c, double ir) {
     this->owner = c;
     this->partner = nullptr;
     this->interestRate = ir;
+    this->balance =0;
 }
 
 Account::Account(int n, Client *c, Client *p, double ir) {
@@ -68,6 +73,7 @@ Account::Account(int n, Client *c, Client *p, double ir) {
     this->owner = c;
     this->partner = p;
     this->interestRate = ir;
+    this->balance =0;
 }
 
 bool Account::canWithdraw(double a) {
@@ -93,12 +99,13 @@ void Account::addInterest(){
 class Bank {
 private:
     Client** clients;
-    int cliensCount;
-
+    int clientsCount;
+    int maxClients;
     Account** accounts;
     int accountsCount;
+    int maxAccounts;
 public:
-    Bank(Client** clients, int cliensAmount);
+    Bank(int c, int a);
     ~Bank();
 
     Client* getClient(int code);
@@ -106,16 +113,124 @@ public:
 
     Client* createClient(int code, string name);
 
-    Account* CreateAccount(int n, Client* c);
-    Account* CreateAccount(int n, Client* c, Client *p);
-    Account* CreateAccount(int n, Client* c, double ir);
-    Account* CreateAccount(int n, Client* c, Client *p, double ir);
+    Account* createAccount(int n, Client* c);
+    Account* createAccount(int n, Client* c, Client *p);
+    Account* createAccount(int n, Client* c, double ir);
+    Account* createAccount(int n, Client* c, Client *p, double ir);
 
     void addInterest();
-
+    void print_stats();
 };
-int main(){
 
+Bank::Bank(int c, int a) {
+    this->clients = new Client * [c]();
+    this->clientsCount = 0;
+    this->maxClients = c;
+    this->accounts = new Account * [a]();
+    this->accountsCount = 0;
+    this->maxAccounts = a;
+}
+
+Bank::~Bank(){
+    for (int i=0; i< this->accountsCount; i++){
+        delete [] this->accounts[i];
+    }
+    delete [] this->accounts;
+    for (int i=0; i<clientsCount; i++){
+        delete [] this->clients[i];
+    }
+    delete [] this->clients;
+
+}
+
+Client * Bank::getClient(int code){
+    for (int i=0; i<clientsCount; i++){
+        if(this->clients[i]->getCode() == code)
+            return this->clients[i];
+    }
+    return nullptr;
+}
+
+Account* Bank::getAccount(int number) {
+    for(int i=0; i<accountsCount; i++){
+        if (this->accounts[i]->getNumber() == number)
+            return this->accounts[i];
+
+    }
+    return nullptr;
+}
+
+Client* Bank::createClient(int code, std::string name) {
+    if (this->clientsCount >= this->maxClients)
+        return nullptr;
+    this->clients[clientsCount] = new Client(name, code);
+
+    this->clientsCount++;
+    return this->clients[clientsCount-1];
+}
+
+Account * Bank::createAccount(int n, Client *c) {
+    if(this->accountsCount >= this->maxAccounts)
+        return nullptr;
+
+    this->accounts[this->accountsCount] =new Account(n,c);
+    this->accountsCount ++;
+    return this->accounts[this->accountsCount-1];
+}
+
+Account * Bank::createAccount(int n, Client *c, Client *p) {
+    if(this->accountsCount >= this->maxAccounts)
+        return nullptr;
+
+    this->accounts[this->accountsCount] =new Account(n,c,p);
+    this->accountsCount ++;
+    return this->accounts[this->accountsCount-1];
+}
+Account * Bank::createAccount(int n, Client *c, double ir) {
+    if(this->accountsCount >= this->maxAccounts)
+        return nullptr;
+
+    this->accounts[this->accountsCount] =new Account(n,c,ir);
+    this->accountsCount ++;
+    return this->accounts[this->accountsCount-1];
+}
+
+Account * Bank::createAccount(int n, Client *c, Client* p, double ir) {
+    if(this->accountsCount >= this->maxAccounts)
+        return nullptr;
+
+    this->accounts[this->accountsCount] =new Account(n,c,p,ir);
+    this->accountsCount ++;
+    return this->accounts[this->accountsCount-1];
+}
+
+void Bank::addInterest() {
+    for(int i=0;i < this->accountsCount; i++) {
+        this->accounts[i]->addInterest();
+    }
+}
+
+void Bank::print_stats() {
+    cout<< endl<< "Accounts:"  << endl;
+    for (int i=0; i< this->accountsCount; i++){
+        cout << "Number: " << this->accounts[i]->getNumber() << " Balance: " << this->accounts[i]->getBalance() << " Owner: " << this->accounts[i]->getOwner()->getName()<< endl;
+    }
+    cout << endl<< "Clients:"  << endl;
+
+    for (int i=0; i<clientsCount; i++){
+        cout << "Code: " << this->clients[i]->getCode() << " Name: " << this->clients[i]->getName() << endl;
+}
+}
+
+int main(){
+    Bank* myBank = new Bank(10,10);
+
+    myBank->createAccount(1, myBank->createClient(1, "Vasya"));
+    myBank->createAccount(2, myBank->createClient(1, "Petya"), myBank->createClient(3, "Petya2"));
+    myBank->createAccount(3, myBank->getClient(3));
+    myBank->print_stats();
+
+    //delete myBank;
     return  0;
 }
 
