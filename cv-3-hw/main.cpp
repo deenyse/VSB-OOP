22f6809025f0deb720accb78b8db6198529abb7c
denvery31 <denvery31@gmail.com>
2024-03-20 13:41:41 +0100

    cv-3, cv-3hw

---
+++ cv-3-hw/main.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+using namespace std;
+
+class Osoba{
+private:
+    string name;
+    string surname;
+    string address;
+
+public:
+    Osoba(string name, string surname, string address);
+    string getName() { return name; }
+    string getSurname() { return surname; }
+    string getAddress() { return address; }
+};
+
+Osoba::Osoba(string name, string surname, string address){
+    this->name = name;
+    this->surname = surname;
+    this->address = address;
+}
+
+class Poloha{
+private:
+    string name;
+    double cost;
+public:
+    Poloha(string name, double cost);
+    string getName() { return name; }
+    double getCost() { return cost; }
+
+};
+
+Poloha::Poloha(string name, double cost){
+    this->name = name;
+    this->cost = cost;
+}
+
+class Factura{
+private:
+    int cislo;
+    Osoba* prijemec;
+    int pocetPolozek;
+    int maxPocetPolozek;
+    Poloha ** polozky;
+
+public:
+    Factura(int cislo, string prijemec_name, string  prijemec_adress, string prijemec_surname, int maxPocetPolozek);
+    ~Factura();
+
+    bool pridatPolozku(string name, double cost);
+    Poloha* odebratPolozku(string name);
+    double spocitatCenu();
+    void vytisknout();
+    int getMaxPocetPolozek() { return this->maxPocetPolozek; }
+};
+
+Factura::Factura(int cislo, std::string prijemec_name,std::string prijemec_surname, std::string prijemec_adress,  int maxPocetPolozek) {
+    this -> cislo = cislo;
+    this -> prijemec = new Osoba(prijemec_name, prijemec_surname, prijemec_adress);
+    this-> pocetPolozek =0;
+    this -> maxPocetPolozek = maxPocetPolozek;
+    this -> polozky = new Poloha * [maxPocetPolozek]();
+}
+
+Factura::~Factura(){
+    for (int i =0; i< this-> pocetPolozek; i++){
+        delete this->polozky[i];
+    }
+
+    delete[] this->polozky;
+}
+
+bool Factura::pridatPolozku(std::string name, double cost) {
+    if (this -> pocetPolozek < this -> maxPocetPolozek){
+        this->polozky[this->pocetPolozek] = new Poloha(name, cost);
+        this->pocetPolozek++;
+        return true;
+    }
+    else
+        return false;
+}
+
+Poloha * Factura::odebratPolozku(std::string name) {
+    Poloha * tmp = nullptr;
+
+    for (int i = 0; i < this -> pocetPolozek; i++){
+        if (this->polozky[i]->getName() == name){ /// stirng compare fix
+            tmp = polozky[i];
+            for (int j = i;  j < this->pocetPolozek -1; j++){
+                this->polozky[j] = this->polozky[j+1];
+            }
+            this->pocetPolozek --;
+            break;
+        }
+        // добавить сдвиг масива
+    }
+    return tmp;
+}
+
+double Factura::spocitatCenu() {
+    double totalCost = 0;
+    for (int i = 0; i < this -> pocetPolozek; i ++){
+        totalCost += polozky[i]->getCost();
+    }
+
+    return totalCost;
+}
+
+void Factura::vytisknout() {
+    cout << "Factura: " << endl;
+    cout << "Cislo:" << this->cislo << endl;
+    cout << "Prijemec" << endl;
+    cout << "\tname: " << this->prijemec->getName() << endl;
+    cout << "\tsurname: " << this->prijemec->getSurname() << endl;
+    cout << "\tadress: " << this->prijemec->getAddress() << endl;
+    cout << "Polozky:" << endl;
+    for (int i = 0; i < this-> pocetPolozek; i++){
+        cout << "\t" << this->polozky[i]->getName() << " " << this->polozky[i]->getCost() << endl;
+    }
+
+    cout << "Tolal: " << this->spocitatCenu() << endl;
+}
+int main (){
+    auto *myFactura = new Factura(123456, "Vasya", "Peckin", "simpleadress", 12 );
+
+    for (int i = 0; i < myFactura->getMaxPocetPolozek(); i++){
+        myFactura->pridatPolozku("Item", i);
+    }
+
+    myFactura->odebratPolozku("Item");
+    myFactura->pridatPolozku("NewItem", 10);
+    myFactura->odebratPolozku("NewItem");
+    myFactura->odebratPolozku("Item");
+    myFactura->pridatPolozku("NewItem", 20);
+    myFactura->vytisknout();
+    return 0;
+}
--- cv-3/main.cpp
+++ cv-3/main.cpp
@@ -1,7 +1,110 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+class KeyValue{
+    private:
+        int key;
+        double value;
+    public:
+        KeyValue(int key, double value);
+        int GetKey();
+        double GetValue();
+};
+
+
+class KeyValues{
+    private:
+        KeyValue** keyValues;
+        int count;
+    public:
+        KeyValues(int n);
+        ~KeyValues();
+        KeyValue* createObject(int key, double value);
+        KeyValue* searchObject(int key);
+        KeyValue* deleteObject(int key);
+        // debug
+        KeyValue ** getKeyValues(){
+            return keyValues;
+        }
+        int Count();
+};
+
+
+KeyValue::KeyValue(int key, double value) {
+    this -> key = key;
+    this -> value = value;
+}
+
+int KeyValue::GetKey(){
+    return this -> key;
+}
+
+double KeyValue::GetValue() {
+    return this -> value;
+}
+
+KeyValues::KeyValues(int n){
+    this-> keyValues = new KeyValue*[n]();
+    this -> count = 0;
+}
+
+KeyValues::~KeyValues(){
+    for (int i = 0; i < this->count; i++){
+        delete this->keyValues[i];
+    }
+
+    delete[] this->keyValues;
+}
+
+
+int KeyValues::Count(){
+    return this->count;
+}
+
+KeyValue* KeyValues::createObject(int key, double value){
+    this->keyValues[this->count] = new KeyValue(key, value);
+    this ->count++;
+    return this->keyValues[this->count-1];
+}
+
+KeyValue* KeyValues::searchObject(int key) {
+    for(int i = 0; i < this->count; i++) {
+        if (this-> keyValues[i]->GetKey() == key){
+            return this->keyValues[i];
+        }
+    }
+    return nullptr;
+}
+
+KeyValue* KeyValues::deleteObject(int key){
+    KeyValue* tmp = nullptr;
+    for (int i = 0; i < this->count; i++) {
+        if (this-> keyValues[i]->GetKey() == key){
+            tmp = keyValues[i];
+            this -> count --;
+            for (int j = i; j < this->count; j++) {
+                this -> keyValues[j] = this -> keyValues[j+1];
+            }
+            break;
+        }
+    }
+    return  tmp;
+}
+int main (){
+    KeyValues* valuesArray = new KeyValues(10);
+    for (int i = 0; i < 5; i++){
+        valuesArray->createObject(i, 100+i);
+    }
+
+    valuesArray->deleteObject(2);
+    valuesArray->deleteObject(1);
+    valuesArray->deleteObject(2);
+
+    for (int i = 0; i < valuesArray->Count(); i++){
+        cout << valuesArray->getKeyValues()[i]->GetKey() << endl;
+    }
+
+
+    delete valuesArray;
     return 0;
 }
