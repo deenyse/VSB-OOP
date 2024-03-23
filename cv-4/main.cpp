9cdc426b50ef49e0d59678b3bc282f9baeddad5b
denvery31 <denvery31@gmail.com>
2024-03-23 20:38:11 +0100

    cv-4.3

--- cv-4/main.cpp
+++ cv-4/main.cpp
@@ -213,24 +213,42 @@ void Bank::addInterest() {
 void Bank::print_stats() {
     cout<< endl<< "Accounts:"  << endl;
     for (int i=0; i< this->accountsCount; i++){
-        cout << "Number: " << this->accounts[i]->getNumber() << " Balance: " << this->accounts[i]->getBalance() << " Owner: " << this->accounts[i]->getOwner()->getName()<< endl;
+        cout << "Number: " << this->accounts[i]->getNumber() << " Balance: " << this->accounts[i]->getBalance() << " ir: " << this->accounts[i]->getInterestRate() * 100 << "% Owner: " << this->accounts[i]->getOwner()->getName();
+        if (this->accounts[i]->getPartner() != nullptr)
+            cout << " Partner: " << this->accounts[i]->getPartner()->getName() << " code: " << this->accounts[i]->getPartner()->getCode();
+        cout << endl;
     }
     cout << endl<< "Clients:"  << endl;
 
     for (int i=0; i<clientsCount; i++){
         cout << "Code: " << this->clients[i]->getCode() << " Name: " << this->clients[i]->getName() << endl;
-}
+    }
 }
 
 int main(){
-    Bank* myBank = new Bank(10,10);
+    Bank* monoBank = new Bank(10,10);
 
-    myBank->createAccount(1, myBank->createClient(1, "Vasya"));
-    myBank->createAccount(2, myBank->createClient(1, "Petya"), myBank->createClient(3, "Petya2"));
-    myBank->createAccount(3, myBank->getClient(3));
-    myBank->print_stats();
+    monoBank->createAccount(1, monoBank->createClient(1, "Vasya"))->Deposit(100);
+    
+    monoBank->createAccount(2, monoBank->createClient(2, "Petya"), monoBank->createClient(3, "Petya2"))->Deposit(120);
+    monoBank->createAccount(3, monoBank->getClient(3))->Deposit(200);
+    monoBank->createAccount(4, monoBank->createClient(5, "Kolya"), monoBank->getClient(1), 0.1)->Deposit(300);
+    monoBank->createAccount(5, monoBank->createClient(4,"koala"), 0.035)->Deposit(10);
+    for (int i = 6; i<=10; i++)
+        monoBank->createAccount(i, monoBank->createClient(i, "Name"))->Deposit(1000);
+    // use gwtAccount ad withdraw
+    monoBank->createAccount(11, monoBank->createClient(11, "Name1")); // acccount is not added
 
-    //delete myBank;
+    monoBank->getAccount(5)->Deposit(1000); // added to koala 1000
+    monoBank->getAccount(5)->Withdraw(2000); // cannot withdraw
+    monoBank->getAccount(10)->Withdraw(1000); // account n10 can't withdraw
+
+
+    monoBank->addInterest();
+    monoBank->addInterest();
+    monoBank->print_stats();
+
+    //delete monoBank;
     return  0;
 }
 
